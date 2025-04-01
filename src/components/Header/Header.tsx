7cb95cd83a20911c4b9e1c78b65837bb088c98b7
TUSHAR THAKRE <tusharthakre2361@gmail.com>
2025-04-01 17:20:39 +0530

    new changes

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,7 +2,7 @@ import { Container } from './styles'
 import { BrowserRouter as Router } from 'react-router-dom'
 import { NavHashLink, HashLink } from 'react-router-hash-link'
 import { useState } from 'react'
-import Resume from '../../assets/Tushar_Thakre_FrontEnd.pdf'
+import Resume from '../../assets/Tushar_Thakre_Frontend_developer.pdf'
 export function Header() {
   const [isActive, setActive] = useState(false)
   function toggleTheme() {
